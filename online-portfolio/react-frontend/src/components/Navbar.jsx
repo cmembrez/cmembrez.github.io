099d0541c4fb53e21faaa90de3a034a73fd14b90
cmembrez <cedric.membrez@gmail.com>
2024-10-20 11:46:50 +0200

    Changed the navbar to minimized hamburger style

--- online-portfolio/react-frontend/src/components/Navbar.jsx
+++ online-portfolio/react-frontend/src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import './Navbar.css';
+import { useState } from 'react';
+
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <>
             <div className="Nav">
-                <div className="Bars" />
-                <div className="NavMenu">
-                    <a className="NavLink" href="#home">
-                        Home
-                    </a>
-                    <a className="NavLink" href="#portfolio">
-                        Portfolio
-                    </a>
+                <div className="Bars" onClick={toggleMenu}>
+                    &#9776; {/* the 3 lines hamburger icon */}
+                </div>
+                <div className={`NavMenu ${menuOpen ? "open" : ""}`}>
+                    <a className="NavLink" href="#home">Home</a>
+                    <a className="NavLink" href="#portfolio">Portfolio</a>
                 </div>
             </div>
         </>
