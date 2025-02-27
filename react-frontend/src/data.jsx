3e7a54a46472f457b0f2fdc38f3018c896630b07
cmembrez <cedric.membrez@gmail.com>
2025-02-27 08:54:19 +0100

    Removed junior. Improved projects' description

--- react-frontend/src/data.jsx
+++ react-frontend/src/data.jsx
@@ -17,7 +17,7 @@ import linkedInLogo from './assets/socialLinks/LI-In-Bug.png';
 
 export const textAboutMe = [
     {
-        shortIntro: "Junior software engineer with hands-on experience in C#, Python, Java and SQL, dedicated to continuous learning and improvement. I am driven to contribute to innovative projects while enhancing my skills in a collaborative environment.",
+        shortIntro: "Software engineer with hands-on experience in C#/.Net, Python, Java and SQL, dedicated to continuous learning and improvement. I am driven to contribute to innovative projects while enhancing my skills in a collaborative environment.",
         longIntro: [
             "I possess a growing foundation in various programming languages and technologies, fueled by a passion for problem-solving. Despite being early in my software engineering career, I have gained practical experience that enables me to contribute effectively across various industries.",
             "Currently, I am working on Industry 4.0 projects, using Python to automate CAD workflow processes and C# to build AR/VR applications. My curiosity extends into areas like cloud computing, big data, and machine learning, and I am excited to further explore these technologies to deliver impactful solutions.",
@@ -37,14 +37,14 @@ export const projects = [
         title: "AR Escape Room",
         subtitle: "Augmented Reality",
         description:
-            "Explore the use of augmented reality, Microsoft HoloLens 2, with people with visual impairments.",
+            "Conducted an exploratory study in collaboration with an association for people with visual impairments. Designed and implemented gamified tasks using augmented reality to assess usability and accessibility.",
         image: projectAREscapeRoomMainImage
     },
     {
         title: "Caius Adventures VR",
         subtitle: "Virtual Reality",
         description:
-            "My very first VR video game using a Quest 2.",
+            "Developed a low-poly virtual reality adventure game using Unity and Blender. Created all assets and textures, resulting in an immersive gaming experience.",
         image: projectCaiusAdventuresVRMainImage
     },
     {
